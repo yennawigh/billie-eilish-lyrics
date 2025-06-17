00b117c2de1dfdfaddd7d38707d8311d407e3147
Ömer <74078118+yennawigh@users.noreply.github.com>
2025-06-18 01:41:39 +0300

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def get_clean_lyrics(url):
     headers = {
-        "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
     }
     try:
         response = requests.get(url, headers=headers, timeout=10)
